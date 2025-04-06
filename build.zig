cd633ce043ba04052617db0f85882020ebd6b3e8
Mediacom <bertoliedoardo99@gmail.it>
2025-04-06 18:56:16 +0200

    added libvaxis dependency for tui

--- build.zig
+++ build.zig
@@ -2,17 +2,22 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
+    const vaxis = b.dependency("vaxis", .{
+        .optimize = optimize,
+        .target = target,
+    });
     const exe_module = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .optimize = optimize,
         .target = target,
     });
+    exe_module.addImport("vaxis", vaxis.module("vaxis"));
+    
     const exe = b.addExecutable(.{
-        .name = "zhist",
         .root_module = exe_module,
+        .name = "zhist",
     });
 
     b.installArtifact(exe);
@@ -20,9 +25,11 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_exe.addArgs(args);
     }
+
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_exe.step);
 
+    // Docs
     const install_docs = b.addInstallDirectory(.{
         .source_dir = exe.getEmittedDocs(),
         .install_dir = .prefix,
